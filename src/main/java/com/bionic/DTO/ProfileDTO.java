79625391546f162bd73a8ce3d830bd3310c068ae
SergiySeleznyov <SergiySeleznyov@gmail.com>
2015-12-11 23:28:51 +0200

    adding controller and DTO for userPage

---
+++ src/main/java/com/bionic/DTO/ProfileDTO.java
@@ -0,0 +1,63 @@
+package com.bionic.DTO;
+
+/**
+ * Created by Sergiy on 08.12.2015.
+ */
+public class ProfileDTO {
+    private long userId;
+    private String firstName;
+    private String lastName;
+
+
+    public ProfileDTO(long userId, String firstName, String lastName) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+
+        return userId;
+    }
+
+    public void setId(long id) {
+        this.userId = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProfileDTO that = (ProfileDTO) o;
+
+        if (userId != that.userId) return false;
+        if (!firstName.equals(that.firstName)) return false;
+        return lastName.equals(that.lastName);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (userId ^ (userId >>> 32));
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        return result;
+    }
+
+}
---
+++ src/main/java/com/bionic/controllers/UserController.java
@@ -0,0 +1,35 @@
+package com.bionic.controllers;
+
+import com.bionic.DAO.UserDAO;
+import com.bionic.DTO.ProfileDTO;
+import com.bionic.DTO.UserDTO;
+import com.bionic.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Sergiy on 08.12.2015.
+ */
+@RestController
+@RequestMapping("/profile")
+public class UserController {
+    @Autowired
+    UserDAO userDAO;
+
+    @RequestMapping(value = "/currentUser", method = RequestMethod.GET, produces = "application/json")
+    public
+    @ResponseBody
+    ResponseEntity<ProfileDTO> getCurrentUserInfo(){
+        Authentication auth= SecurityContextHolder.getContext().getAuthentication();
+        User currentUser=userDAO.getUserByEmail(auth.getName());
+        ProfileDTO profileDTO=new ProfileDTO(currentUser.getId(),currentUser.getFirstName(),currentUser.getLastName());
+        return new ResponseEntity<>(profileDTO,HttpStatus.OK);
+    }
+}
