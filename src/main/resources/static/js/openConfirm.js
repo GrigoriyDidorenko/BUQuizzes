8d5971ca550553d1c7ae7b27d81c0997cdd91a79
whoover.89 <whoover.89@gmail.com>
2016-01-13 21:49:36 +0200

    add style to onetimetests

--- src/main/resources/static/js/openConfirm.js
+++ src/main/resources/static/js/openConfirm.js
@@ -14,5 +14,7 @@ $(document).ready(function ($) {
             }
             var mark = GetURLParameter('mark');
             $("#mark").append(mark);
+    var email = GetURLParameter('email');
+    $("#email").append(email);
 
 });
