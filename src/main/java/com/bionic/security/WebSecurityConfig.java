fee599150b5a197f39190cb167954bed9aec96db
SergiySeleznyov <SergiySeleznyov@gmail.com>
2016-02-16 21:18:47 +0200

    "adding redirect strategy and permissions in security"

--- src/main/java/com/bionic/security/WebSecurityConfig.java
+++ src/main/java/com/bionic/security/WebSecurityConfig.java
@@ -1,18 +1,19 @@
-package com.bionic.security;
 
-import com.bionic.security.CustomUserDetailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+        package com.bionic.security;
+
+        import com.bionic.security.CustomUserDetailService;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.boot.autoconfigure.security.SecurityProperties;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.core.annotation.Order;
+        import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+        import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+        import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+        import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 /**
  * Created by Sergiy on 24.11.2015.
@@ -33,12 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(getMd5PasswordEncoder());
 
     }
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("superadmin@gmail.com").password("root").roles("SUPERADMIN");
-    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -55,23 +51,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         http.logout()
-                // разрешаем делать логаут всем
+// разрешаем делать логаут всем
                 .permitAll()
-                        // указываем URL логаута
+// указываем URL логаута
                 .logoutUrl("/logout")
-                        // указываем URL при удачном логауте
+// указываем URL при удачном логауте
                 .logoutSuccessUrl("/pages/LoginPage.html")
-                        // делаем не валидной текущую сессию
+// делаем не валидной текущую сессию
                 .invalidateHttpSession(true);
 
-        http.authorizeRequests();
-                //.antMatchers("/").authenticated()
-//                .antMatchers("/pages/addUser.html").access("hasRole('SUPERADMINISTRATOR')");
+        http.authorizeRequests()
+                .antMatchers("/").authenticated()
+                .antMatchers("/pages/openTests/**").permitAll()
+                .antMatchers("/pages/addUser.html").access("hasAnyRole('SUPERADMIN, TRAINER')")
+                .antMatchers("/pages/addTest.html").access("hasRole('TRAINER')")
+                .antMatchers("/pages/addTestToUser.html").access("hasRole('TRAINER')")
+                .antMatchers("/pages/allOpenQuestions.html").access("hasRole('TRAINER')")
+                .antMatchers("/pages/auditOpenQuestion.html").access("hasRole('TRAINER')")
+                .antMatchers("/pages/editorForTest.html").access("hasRole('TRAINER')")
+                .antMatchers("/pages/editTest.html").access("hasRole('TRAINER')")
+                .antMatchers("/pages/importTest.html").access("hasRole('TRAINER')")
+                .antMatchers("/pages/mentorPage.html").access("hasRole('TRAINER')")
+                .antMatchers("/pages/TestPage.html").access("hasAnyRole('STUDENT','TRAINER')")
+                .antMatchers("/pages/userPage.html").access("hasAnyRole('STUDENT','TRAINER')");
 
 
 
     }
-
 
     @Bean
     public Md5PasswordEncoder getMd5PasswordEncoder(){
